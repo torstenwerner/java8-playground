28c6ba92490eecc68190f25349bb3c2c32479d33
Torsten Werner <torsten.werner@westernacher.com>
2014-12-27 15:48:37 +0100

    add performance checks

---
+++ src/test/java/CheckOptional.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class CheckOptional {
+    @Test
+    public void testOptional() throws Exception {
+        Optional.ofNullable("Hallo");
+
+    }
+}
