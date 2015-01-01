a874ffa00e1031aef8d80168135a32cff50944c3
Torsten Werner <torsten.werner@westernacher.com>
2015-01-02 00:07:08 +0100

    fillInStackTrace()

--- src/test/java/ExceptionSupplier.java
+++ src/test/java/ExceptionSupplier.java
@@ -15,6 +15,17 @@ public class ExceptionSupplier {
     }
 
     @Test
+    public void testJava7StyleExtra() throws Throwable {
+        final Exception exception = new RuntimeException("some message");
+        try {
+            System.out.println("The real work is done here.");
+            throw exception.fillInStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void testJava8Style() throws Exception {
         final Supplier<Exception> exceptionSupplier = () -> new RuntimeException("some message");
         try {
