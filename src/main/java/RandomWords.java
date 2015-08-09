26a77357c49fa15b631365a4c7525c3cbc248b80
Torsten Werner <torsten.werner@westernacher.com>
2015-08-09 10:42:47 +0200

    implement Lesson3

--- src/main/java/RandomWords.java
+++ src/main/java/RandomWords.java
@@ -1,11 +1,13 @@
 /**
  * Copyright © 2014, Oracle and/or its affiliates. All rights reserved.
- * 
+ * <p>
  * JDK 8 MOOC Lesson 3 homework
  */
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
@@ -19,40 +21,52 @@ import java.util.stream.Collectors;
  * @author Simon Ritter (@speakjava)
  */
 public class RandomWords {
-  private final List<String> sourceWords;
+    private final List<String> sourceWords;
 
-  /**
-   * Constructor
-   * 
-   * @throws IOException If the source words file cannot be read
-   */
-  public RandomWords() throws IOException {   
-    try (BufferedReader reader = Files.newBufferedReader(Paths.get("words"))) {
-      sourceWords = null;    // YOUR CODE HERE
-      
-      System.out.println("Loaded " + sourceWords.size() + " words");
+    /**
+     * Constructor
+     *
+     * @throws IOException If the source words file cannot be read
+     */
+    public RandomWords() throws IOException {
+        final URI uri;
+        try {
+            uri = getClass().getResource("Lesson3.txt").toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(uri))) {
+            sourceWords = reader.lines()
+                    .map(String::trim)
+                    .collect(Collectors.toList());
+
+            System.out.println("Loaded " + sourceWords.size() + " words");
+        }
     }
-  }
 
-  /**
-   * Create a list of a given size containing random words
-   *
-   * @param listSize The size of the list to create
-   * @return The created list
-   */
-  public List<String> createList(int listSize) {
-    Random rand = new Random();
-    List<String> wordList = null; // YOUR CODE HERE
+    /**
+     * Create a list of a given size containing random words
+     *
+     * @param listSize The size of the list to create
+     * @return The created list
+     */
+    public List<String> createList(int listSize) {
+        if (listSize < 1) {
+            throw new RuntimeException("invalid value for parameter listSize: " + listSize);
+        }
+        Random rand = new Random();
 
-    return wordList;
-  }
+        return rand.ints(listSize, 0, sourceWords.size())
+                .mapToObj(sourceWords::get)
+                .collect(Collectors.toList());
+    }
 
-  /**
-   * Return the list of all source words, which cannot be modified
-   *
-   * @return The unmodifiable list of all source words
-   */
-  public List<String> allWords() {
-    return Collections.unmodifiableList(sourceWords);
-  }
+    /**
+     * Return the list of all source words, which cannot be modified
+     *
+     * @return The unmodifiable list of all source words
+     */
+    public List<String> allWords() {
+        return Collections.unmodifiableList(sourceWords);
+    }
 }
