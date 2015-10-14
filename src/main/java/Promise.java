483d4d1b9bca9cf34e4039b46d31d16375f4ad98
Torsten Werner <torsten.werner@westernacher.com>
2015-10-14 20:11:55 +0200

    make promise tasks asynchronous

--- src/main/java/Promise.java
+++ src/main/java/Promise.java
@@ -1,21 +1,23 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 public class Promise {
     private CompletableFuture<String> create(String text) {
-        final CompletableFuture<String> promise = new CompletableFuture<>();
-        try {
-            Thread.sleep(100);
-            promise.complete(text.toUpperCase());
-        } catch (InterruptedException e) {
-            promise.completeExceptionally(e);
-        }
-        return promise;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(100);
+                return text;
+            } catch (InterruptedException e) {
+                throw new CompletionException(e);
+            }
+        });
     }
 
     private void run() {
         create("hello")
                 .thenCompose(word -> create(word + " world!"))
-                .thenAccept(System.out::println);
+                .thenAccept(System.out::println)
+                .join();
     }
 
     public static void main(String args[]) {
