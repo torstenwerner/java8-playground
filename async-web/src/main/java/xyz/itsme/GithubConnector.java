f5a7076ae30d7757fec00eca0113d3603b2e0bf3
Torsten Werner <torsten.werner@westernacher.com>
2015-10-18 09:15:05 +0200

    use type reference

--- async-web/src/main/java/xyz/itsme/GithubConnector.java
+++ async-web/src/main/java/xyz/itsme/GithubConnector.java
@@ -1,5 +1,8 @@
 package xyz.itsme;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +17,15 @@ public class GithubConnector {
 
     public CompletableFuture<Map<String, URL>> root() {
         final CompletableFuture<Map<String, URL>> promise = new CompletableFuture<>();
-        promise.complete(restOperations.getForObject("https://api.github.com/", Map.class));
+        promise.complete(callGithub().getBody());
         return promise;
+    }
+
+    private final ParameterizedTypeReference<Map<String, URL>> responseType =
+            new ParameterizedTypeReference<Map<String, URL>>() {
+            };
+
+    private ResponseEntity<Map<String, URL>> callGithub() {
+        return restOperations.exchange("https://api.github.com/", HttpMethod.GET, null, responseType);
     }
 }
