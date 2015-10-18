e18f2a4fe247ddd7685189192d2ba42a28cdda1d
Torsten Werner <torsten.werner@westernacher.com>
2015-10-18 12:49:42 +0200

    configure request timeouts and connection pool size

--- async-web/src/main/java/xyz/itsme/GithubConnector.java
+++ async-web/src/main/java/xyz/itsme/GithubConnector.java
@@ -1,8 +1,13 @@
 package xyz.itsme;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.AsyncClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestOperations;
@@ -14,7 +19,26 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class GithubConnector {
-    private final AsyncRestOperations restOperations = new AsyncRestTemplate();
+    private final AsyncRestOperations restOperations;
+
+    public GithubConnector() {
+        // set timeouts in milliseconds
+        final RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(10000)
+                .setConnectTimeout(10000)
+                .setSocketTimeout(60000)
+                .build();
+
+        // set request configuration and connection pool size
+        final CloseableHttpAsyncClient asyncClient = HttpAsyncClientBuilder.create()
+                .setDefaultRequestConfig(requestConfig)
+                .setMaxConnPerRoute(10)
+                .setMaxConnTotal(30)
+                .build();
+
+        final AsyncClientHttpRequestFactory requestFactory = new HttpComponentsAsyncClientHttpRequestFactory(asyncClient);
+        restOperations = new AsyncRestTemplate(requestFactory);
+    }
 
     public CompletableFuture<Map<String, URL>> root() {
         final CompletableFuture<Map<String, URL>> promise = new CompletableFuture<>();
