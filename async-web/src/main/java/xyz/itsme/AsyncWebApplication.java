3e44bda7ab2e1c09627e1fbc91d28271098d5d3e
Torsten Werner <torsten.werner@westernacher.com>
2015-10-18 08:49:07 +0200

    add new module async-web

---
+++ async-web/src/main/java/xyz/itsme/AsyncWebApplication.java
@@ -0,0 +1,12 @@
+package xyz.itsme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncWebApplication.class, args);
+    }
+}
